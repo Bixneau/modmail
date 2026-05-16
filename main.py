894bf0465942b1b5be6bc5b1059d09f46b6bc5ea
Bixneau <161073583+Bixneau@users.noreply.github.com>
2026-05-17 00:40:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ import discord
 from discord.ext import commands
 import os
 
+# Token sécurisé Render
 TOKEN = os.getenv("TOKEN")
 
+# Intents
 intents = discord.Intents.default()
 intents.message_content = True
+intents.guilds = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+# Bot
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
 
+# Bot prêt
 @bot.event
 async def on_ready():
 
@@ -21,8 +29,82 @@ async def on_ready():
 
     print(f"Connecté en tant que {bot.user}")
 
+# Commande ticket
 @bot.command()
-async def ping(ctx):
-    await ctx.send("Pong 🏓")
+async def ticket(ctx):
 
+    # ID catégorie tickets
+    category_id = 1505338661078696046
+
+    # ID salon logs
+    logs_channel_id = 1505338942633807995
+
+    guild = ctx.guild
+
+    # Récupère catégorie
+    category = discord.utils.get(
+        guild.categories,
+        id=category_id
+    )
+
+    # Création salon
+    channel = await guild.create_text_channel(
+        name=f"ticket-{ctx.author.name}",
+        category=category
+    )
+
+    # Ping utilisateur
+    await channel.send(
+        f"{ctx.author.mention}"
+    )
+
+    # Embed ticket
+    embed = discord.Embed(
+        title="🎫 Nouveau Ticket",
+        description="Le support va bientôt vous répondre.",
+        color=discord.Color.blurple()
+    )
+
+    embed.add_field(
+        name="Utilisateur",
+        value=ctx.author.mention,
+        inline=False
+    )
+
+    await channel.send(embed=embed)
+
+    # Logs
+    logs_channel = bot.get_channel(
+        logs_channel_id
+    )
+
+    if logs_channel:
+
+        log_embed = discord.Embed(
+            title="📁 Ticket Créé",
+            color=discord.Color.green()
+        )
+
+        log_embed.add_field(
+            name="Utilisateur",
+            value=ctx.author.mention,
+            inline=False
+        )
+
+        log_embed.add_field(
+            name="Salon",
+            value=channel.mention,
+            inline=False
+        )
+
+        await logs_channel.send(
+            embed=log_embed
+        )
+
+    # Confirmation
+    await ctx.send(
+        f"✅ Ticket créé : {channel.mention}"
+    )
+
+# Lancement bot
 bot.run(TOKEN)
